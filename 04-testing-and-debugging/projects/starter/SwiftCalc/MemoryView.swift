475ce765010b72e7edf9e7e9acada8ea9c277247
Guilherme Amaral Dourado <guilhermeamaraldourado@gmail.com>
2023-11-23 20:22:25 -0300

    completed

--- 04-testing-and-debugging/projects/starter/SwiftCalc/MemoryView.swift
+++ 04-testing-and-debugging/projects/starter/SwiftCalc/MemoryView.swift
@@ -1,15 +1,15 @@
 /// Copyright (c) 2023 Kodeco Inc.
-/// 
+///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-/// 
+///
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-/// 
+///
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-/// 
+///
 /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,25 +29,55 @@
 import SwiftUI
 
 struct MemoryView: View {
-  @Binding var memory: Double
-  var geometry: GeometryProxy
-
-  var body: some View {
-    HStack {
-      Spacer()
-      Text("\(memory)")
-        .padding(.horizontal, 5)
-        .frame(
-          width: geometry.size.width * 0.85,
-          alignment: .trailing
-        )
-        .overlay(
-          RoundedRectangle(cornerRadius: 8)
-            .stroke(lineWidth: 2)
-            .foregroundColor(Color.gray)
-        )
-      // Add gesture here
-      Text("M")
-    }.padding(.bottom).padding(.horizontal, 5)
-  }
+    @Binding var memory: Double
+    var geometry: GeometryProxy
+    
+    var body: some View {
+#if targetEnvironment(macCatalyst)
+        let doubleTap = TapGesture(count: 2)
+            .onEnded { _ in
+                self.memory = 0.0
+            }
+#else
+        let memorySwipe = DragGesture(minimumDistance: 20)
+            .onEnded { _ in
+                memory = 0.0
+            }
+#endif
+        HStack {
+            Spacer()
+#if !targetEnvironment(macCatalyst)
+            Text("\(memory)")
+                .accessibilityIdentifier("memoryDisplay")
+                .padding(.horizontal, 5)
+                .frame(
+                    width: geometry.size.width * 0.85,
+                    alignment: .trailing
+                )
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(lineWidth: 2)
+                        .foregroundColor(Color.gray)
+                )
+                .gesture(memorySwipe)
+#else
+            TextMemory(memory: 0.0, gesture: doubleTap, geometry: geometry)
+            Text("\(memory)")
+              .accessibility(identifier: "memoryDisplay")
+              .padding(.horizontal, 5)
+              .frame(
+                width: geometry.size.width * 0.85,
+                alignment: .trailing
+              )
+              .overlay(
+                RoundedRectangle(cornerRadius: 8)
+                  .stroke(lineWidth: 2)
+                  .foregroundColor(Color.gray)
+              )
+              // Add gesture here
+              .gesture(doubleTap)
+#endif
+            Text("M")
+        }.padding(.bottom).padding(.horizontal, 5)
+    }
 }
